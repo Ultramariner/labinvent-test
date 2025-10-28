54efd03b9f379842a45f60b6012d1c4664cbf24b
Ultramar <artyom.odinochenko@yandex.ru>
2025-10-29 00:07:46 +0300

    Project config and dependencies

--- api/src/main/java/com/labinvent/analyzer/ApiApplication.java
+++ api/src/main/java/com/labinvent/analyzer/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.labinvent.api;
+package com.labinvent.analyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
