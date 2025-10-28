151559595e72ca4a29c82eb927f0ddc4af910ec4
Ultramar <artyom.odinochenko@yandex.ru>
2025-10-29 00:13:41 +0300

    Add repository

---
+++ api/src/main/java/com/labinvent/analyzer/repository/AnalysisRecordRepository.java
@@ -0,0 +1,16 @@
+package com.labinvent.analyzer.repository;
+
+import com.labinvent.analyzer.entity.AnalysisRecord;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AnalysisRecordRepository extends JpaRepository<AnalysisRecord, Long> {
+
+    @Query("SELECT r FROM AnalysisRecord r ORDER BY r.uploadedAt ASC")
+    List<AnalysisRecord> findOldest(Pageable pageable);
+}
